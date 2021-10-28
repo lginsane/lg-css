fd3985f267615a79e871635c896046d9bed2a065
梁傲 <1289206613@qq.com>
2021-10-28 18:32:40 +0800

    fix: 删除dist

--- dist/assets/basics_color.md.32d67e88.lean.js
+++
@@ -1,8 +0,0 @@
-import{_ as o,c as e,o as c,a as r}from"./app.399bd2ee.js";const m='{"title":"\u989C\u8272color","description":"","frontmatter":{},"relativePath":"basics/color.md","lastUpdated":1635414652718}',s={},t=r(`__VP_STATIC_START__<h1 id="\u989C\u8272color" tabindex="-1">\u989C\u8272color <a class="header-anchor" href="#\u989C\u8272color" aria-hidden="true">#</a></h1><p>\u6309\u9700\u4F7F\u7528</p><div class="language-"><pre><code>import &#39;lg-css/dist/color/index.css&#39;
-or
-import &#39;lg-css/color/index&#39;
-</code></pre></div><p>\u81EA\u5B9A\u4E49\u914D\u7F6E</p><div class="language-\u81EA\u5B9A\u4E49\u914D\u7F6E"><pre><code>@import &quot;lg-css/src/mixin.scss&quot;;
-
-$color: (&#39;000&#39;: #000000, &#39;fff&#39;: #ffffff);
-@include customStyle(&#39;color&#39;, $color);
-</code></pre></div><p>\u4F7F\u7528\u4F8B\u5B50\uFF1A <code>.c-000</code>\u3001<code>.c-fff</code></p>__VP_STATIC_END__`,6),a=[t];function d(n,i,l,_,p,f){return c(),e("div",null,a)}var h=o(s,[["render",d]]);export{m as __pageData,h as default};
--- dist/assets/basics_reset.md.b2ed2ed8.lean.js
+++
@@ -1,4 +0,0 @@
-import{_ as t,c as s,o as a,b as e,d as r}from"./app.399bd2ee.js";const g='{"title":"\u91CD\u7F6Ereset","description":"","frontmatter":{},"relativePath":"basics/reset.md","lastUpdated":1635412144893}',o={},n=e("h1",{id:"\u91CD\u7F6Ereset",tabindex:"-1"},[r("\u91CD\u7F6Ereset "),e("a",{class:"header-anchor",href:"#\u91CD\u7F6Ereset","aria-hidden":"true"},"#")],-1),c=e("p",null,"\u6309\u9700\u4F7F\u7528",-1),d=e("div",{class:"language-"},[e("pre",null,[e("code",null,`import 'lg-css/dist/reset/index.css'
-or
-import 'lg-css/reset/index'
-`)])],-1),i=[n,c,d];function l(_,p,h,u,f,m){return a(),s("div",null,i)}var b=t(o,[["render",l]]);export{g as __pageData,b as default};
