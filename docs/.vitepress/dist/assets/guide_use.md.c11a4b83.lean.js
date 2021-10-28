d856bf82fe0e6e1705ac68645c6e33923b65cd8b
梁傲 <1289206613@qq.com>
2021-10-28 18:00:48 +0800

    build: docs

---
+++ docs/.vitepress/dist/assets/guide_use.md.c11a4b83.lean.js
@@ -0,0 +1,6 @@
+import{_ as e,c as s,o as t,a}from"./app.399bd2ee.js";const h='{"title":"\u4F7F\u7528","description":"","frontmatter":{},"relativePath":"guide/use.md","lastUpdated":1635414942119}',o={},c=a(`__VP_STATIC_START__<h1 id="\u4F7F\u7528" tabindex="-1">\u4F7F\u7528 <a class="header-anchor" href="#\u4F7F\u7528" aria-hidden="true">#</a></h1><ol><li>\u5168\u90E8\u5BFC\u5165</li></ol><div class="language-"><pre><code>import &#39;lg-css&#39;
+</code></pre></div><ol start="2"><li>\u6309\u9700\u5BFC\u5165</li></ol><div class="language-"><pre><code>import &#39;lg-css/dist/background/index.css&#39;
+import &#39;lg-css/dist/color/index.css&#39;
+import &#39;lg-css/dist/reset/index.css&#39;
+...
+</code></pre></div>__VP_STATIC_END__`,5),i=[c];function r(d,n,l,_,p,u){return t(),s("div",null,i)}var m=e(o,[["render",r]]);export{h as __pageData,m as default};
