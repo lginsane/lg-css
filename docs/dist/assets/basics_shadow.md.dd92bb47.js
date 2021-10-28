5f2dee769436140f620fd42c56a068be59e7affd
梁傲 <1289206613@qq.com>
2021-10-28 18:29:14 +0800

    fix: dist

--- docs/dist/assets/basics_shadow.md.dd92bb47.js
+++
@@ -1,4 +0,0 @@
-import{_ as t,c as a,o as d,a as o}from"./app.399bd2ee.js";const x='{"title":"\u9634\u5F71 shadow","description":"","frontmatter":{},"relativePath":"basics/shadow.md","lastUpdated":1635412146908}',s={},e=o(`<h1 id="\u9634\u5F71-shadow" tabindex="-1">\u9634\u5F71 shadow <a class="header-anchor" href="#\u9634\u5F71-shadow" aria-hidden="true">#</a></h1><p>\u6309\u9700\u4F7F\u7528</p><div class="language-"><pre><code>import &#39;lg-css/dist/shadow/index.css&#39;
-or
-import &#39;lg-css/shadow/index&#39;
-</code></pre></div><table><thead><tr><th>\u7C7B\u540D</th><th>\u6837\u5F0F</th></tr></thead><tbody><tr><td>.shadow-block</td><td>box-shadow:0 0 4px 2px rgba(0,0,0,.1)</td></tr><tr><td>.shadow-top</td><td>box-shadow:0 -2px 4px 0 rgba(0,0,0,.1)</td></tr><tr><td>.shadow-bottom</td><td>box-shadow:0 2px 4px 0 rgba(0,0,0,.1)</td></tr></tbody></table>`,4),r=[e];function h(c,_,p,n,i,l){return d(),a("div",null,r)}var w=t(s,[["render",h]]);export{x as __pageData,w as default};
